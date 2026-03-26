a72749b75bd94a752700a8f8affb1fe2928bab28
gopalakrishnanplus-creator <gopala.krishnanplus@inditech.co.in>
2026-03-26 15:05:14 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -149,6 +149,7 @@ SOCIALACCOUNT_ADAPTER = 'accounts.adapter.RoleAwareSocialAccountAdapter'
 
 GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID', '')
 GOOGLE_CLIENT_SECRET = os.environ.get('GOOGLE_CLIENT_SECRET', '')
+print("GOOGLE_CLIENT_ID -> ", GOOGLE_CLIENT_ID)
 SOCIALACCOUNT_PROVIDERS = {
     'google': {
         'SCOPE': ['profile', 'email'],
